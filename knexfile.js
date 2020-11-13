c966cbae8a9074f2858dbfec3223ce4c4c12f861
Bruno Paula <devbrunopaula@gmail.com>
2020-11-13 13:09:16 -0500

    heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -19,15 +19,8 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'unit4',
-      user: 'postgres',
-      password: '',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
+    connection: process.env.DATABASE_URL,
+
     migrations: {
       directory: './database/migrations',
       tableName: 'knex_migrations',
